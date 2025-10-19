71496f46b8610952ff54ff918df33cf2996b3264
Jmack66 <mack.jonah@gmail.com>
2025-10-19 13:24:44 +0100

    index

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jmack66.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
